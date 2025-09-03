b4f59ee96b73d72d6eb3bc46e287b5561f97e181
Luke Morrison CS2022 <luke.morrison.2022@uni.strath.ac.uk>
2025-09-03 13:56:48 +0100

    Security fixes

--- sds/firebaseConfig.js
+++ sds/firebaseConfig.js
@@ -1,23 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { getAuth, initializeAuth, getReactNativePersistence,  } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCJ7tDcw7Z2VF8jdafv_vLvN9BydBGUobo",
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
     authDomain: "surfscot-2b29c.firebaseapp.com",
-    projectId: "surfscot-2b29c",
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: "surfscot-2b29c.firebasestorage.app",
     messagingSenderId: "452014169194",
-    appId: "1:452014169194:web:dac40e0965b67fef67be13",
-    measurementId: "G-T6RW42M84K"
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    measurementId: "G-DN0GX8E6S5"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = initializeAuth(app);
+export const db = getFirestore(app);
 const analytics = getAnalytics(app);
